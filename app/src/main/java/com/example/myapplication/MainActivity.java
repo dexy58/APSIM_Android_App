5c353ae86298c0031be2c919ee3d9014ac0e8d26
unknown <ivan.kralj@fer.hr>
2021-12-20 13:08:35 +0100

    Added dates and button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner spinnerLanguages=findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.cultures, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinnerLanguages.setAdapter(adapter);
+
+        DatePicker simpleDatePickerStart = findViewById(R.id.simpleDatePicker); // initiate a date picker
+        simpleDatePickerStart.setSpinnersShown(false); // set false value for the spinner shown function
+
+        DatePicker simpleDatePickerEnd = findViewById(R.id.simpleDatePicker2); // initiate a date picker
+        simpleDatePickerEnd.setSpinnersShown(false); // set false value for the spinner shown function
     }
 }
